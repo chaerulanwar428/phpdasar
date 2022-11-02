990a14cefc9ab8c8de02ed94e8f9ff5b91001113
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-03 01:56:11 +0700

    menambahkan contoh HTML dalam PHP

--- materi04/index.php
+++ materi04/index.php
@@ -32,3 +32,19 @@
 </body>
 </html>
 
+
+<!-- contoh HTML didalam PHP -->
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <?php
+    echo "<h1> Halo, selamat datang chaerul </h1>";
+    ?>
+</body>
+</html> -->
