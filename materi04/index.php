407e3d36f9ace79d3c9d3a4202fc3072503c9bc6
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-03 01:37:58 +0700

    menambahkan contoh file darinoutput standar

---
+++ materi04/index.php
@@ -0,0 +1,14 @@
+<?php
+// pertemuan2
+// sintaks php
+
+// standart output
+// echo, print => echo "chaerul";
+// print_r      => print_r("chaerul");
+//var dumb      => var_dumb("chaerul");
+
+echo "chaerul";
+echo 123;
+echo true;
+echo false;
+?>
