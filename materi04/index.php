857ccf652c03e3c7746d9344c049adb94778a320
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-03 01:50:18 +0700

    menggunakan PHP dalam HTML

--- materi04/index.php
+++ materi04/index.php
@@ -5,10 +5,30 @@
 // standart output
 // echo, print => echo "chaerul";
 // print_r      => print_r("chaerul");
-//var dumb      => var_dumb("chaerul");
+// var dumb      => var_dumb("chaerul");
 
-echo "chaerul";
-echo 123;
-echo true;
-echo false;
+// echo "chaerul";
+// echo 123;
+// echo true;
+// echo false;
+
+// penulisan sintaks PHP
+// 1. PHP didalam HTML 
+// 2. HTML di dalam PHP
 ?>
+
+
+<!-- contoh PHP dalam HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Halo, Selamat datang <?php echo "Chaerul";?></h1>
+</body>
+</html>
+
