627ad01caba302065cd87be977b56b80a3270ef0
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-04 01:00:52 +0700

    menambahkan kasus penyelesaian cara ke-2 penerapan looping pada website

--- materi05/index02.php
+++ materi05/index02.php
@@ -26,13 +26,13 @@
 </body>
 </html> -->
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kasus penyelesaian 1</title>
+    <title>kasus penyelesaian 1 penrapan looping pada website</title>
 </head>
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
@@ -46,5 +46,25 @@
             echo"</tr>";
         }
         ?>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kasus penyelesaian 2 penerapan looping pada website</title>
+</head>
+<body>
+    <table border="1" cellpadding="10" cellspacing="0">
+      <?php for ($i = 1; $i <= 3; $i++ ) : ?>
+        <tr>
+            <?php for ($j = 1; $j <= 5; $j++) : ?>
+                <td> <?php echo "$i, $j"; ?></td>
+            <?php endfor; ?>
+            </tr>
+            <?php endfor; ?>
 </body>
 </html>
