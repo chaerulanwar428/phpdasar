73763c8df8d18a5b90ed604928080d5346cbaf5a
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-03 20:34:00 +0700

    materi pengulangan do while

--- materi05/index.php
+++ materi05/index.php
@@ -9,9 +9,15 @@
 //     echo "Hello World! <br>";
 // }
 
-$i =0;
-while ($i < 5){
+// $i =0;
+// while ($i < 5){
+//     echo "Hello World! <br>";
+//     $i++;
+// }
+
+$i = 10;
+do {
     echo "Hello World! <br>";
     $i++;
-}
+}while($i < 5);
 ?>
