7281e6fd7cb714132a20a80f9ab1d4dabb760997
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-03 20:25:39 +0700

    materi pengulangan while

--- materi05/index.php
+++ materi05/index.php
@@ -5,7 +5,13 @@
 //do... while
 //foreach : pengulangan khusus array
 
-for ( $i = 0; $i < 5; $i++) {
+// for ( $i = 0; $i < 5; $i++) {
+//     echo "Hello World! <br>";
+// }
+
+$i =0;
+while ($i < 5){
     echo "Hello World! <br>";
+    $i++;
 }
 ?>
