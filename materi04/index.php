9f831e7a4f58743f28a523855df9f617331f6a7a
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-03 15:37:30 +0700

    materi aritmatika

--- materi04/index.php
+++ materi04/index.php
@@ -21,6 +21,15 @@
 //tidak boleh diawali dengan angka,tapi boleh mengandung angka
 // $nama="chaerul anwar";
 // echo "halo, nama saya $nama";
+
+// Operator
+// aritmatika
++ - * / %
+$x =10;
+$y = 20;
+echo $x * $y;
+
+
 ?>
 
 
