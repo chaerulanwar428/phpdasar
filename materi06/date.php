acb91f87e06c26875439449e30b7a0f0bb63e6e7
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-12 23:55:36 +0700

    menentukan waktu 1 tahun sebelimnya daris sekarang

--- materi06/date.php
+++ materi06/date.php
@@ -1,3 +1,11 @@
 <?php
-echo date("l, d-M-Y");
+//Date
+//untuk menampilkan tanggal dengan format tertentu
+//echo date("l, d-M-Y");
+
+//Time
+//UNIX Timestamp/EPOCH time
+//detik yang sudah berlalu sejak 1 januari 1970
+//echo time();
+echo date ("l", time()-60*60*24*100);
 ?>
