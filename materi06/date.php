76b6b477b4e284d8954e1efeff0bbdb53feafa1a
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-13 23:28:44 +0700

    menggunkan function mktime

--- materi06/date.php
+++ materi06/date.php
@@ -7,5 +7,11 @@
 //UNIX Timestamp/EPOCH time
 //detik yang sudah berlalu sejak 1 januari 1970
 //echo time();
-echo date ("l", time()-60*60*24*100);
+// echo date ("l", time()-60*60*24*100);
+
+//mktime
+//membuat sendiri detik
+//mktime(0,,0,0,0,0,0)
+//jam, menit, detik, bulan, tanggal, tahun
+echo date ("l", mktime(0,0,0,2,18,1999));
 ?>
