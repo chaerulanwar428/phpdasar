0bf5a4eb3c43c0c84c4610e378d884d7cc870058
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-16 15:16:31 +0700

    menampilkan array pada website

--- materi07/latihan1.php
+++ materi07/latihan1.php
@@ -2,6 +2,8 @@
 //array
 //variabel yang dapat memiliki banyak nilai
 //elemen pada array boleh memiliki tipe data yang berbeda
+//pasangan antara key dan value
+//kepy-nya adalah index, yang dimulai darin0
 
 //membuat array
 //cara lama
@@ -10,5 +12,19 @@ $hari = array("senin", "selasa" , "rabu");
 //cara baru
 $bulan = ["januari", "februari", "maret"];
 $arr1 = [123, "tulisan", false];
+echo"<br>";
 
+
+//menampilkan array
+// var_dump / print_r()
+var_dump($hari);
+echo "br";
+print_r($bulan);
+
+//menampilkan 1 elemen pada array
+echo $arr1[0];
+echo "<br>";
+echo $bulan[1];
+ 
 ?>
+
