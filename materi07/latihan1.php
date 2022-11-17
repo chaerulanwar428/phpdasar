c693cb89937cd7282004b3dce71acdb5a4f1e5b1
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-17 19:48:29 +0700

    menambahkan elemen baru pada array yang sebelumnya dibbuat

--- materi07/latihan1.php
+++ materi07/latihan1.php
@@ -12,19 +12,26 @@ $hari = array("senin", "selasa" , "rabu");
 //cara baru
 $bulan = ["januari", "februari", "maret"];
 $arr1 = [123, "tulisan", false];
-echo"<br>";
+echo "<br>";
 
 
 //menampilkan array
 // var_dump / print_r()
 var_dump($hari);
-echo "br";
+echo "<br>";
 print_r($bulan);
 
 //menampilkan 1 elemen pada array
 echo $arr1[0];
 echo "<br>";
 echo $bulan[1];
+
+//menambakan elemen baru pada array
+var_dump($hari);
+$hari[] = "kamis";
+$hari[] = "jum'at";
+echo "<br>";
+var_dump($hari);
  
 ?>
 
