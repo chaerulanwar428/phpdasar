bb2006d0c085fa4243301b7a6521a32143411e4e
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-19 03:11:50 +0700

    menambahkan file latihan2

---
+++ materi08/latihan2.php
@@ -0,0 +1,21 @@
+<?php
+//array associative
+//definisinya sama seperti array numerik, kecualo
+//key-nya adalah string yang kita buat sendiri
+
+$mahasiswa = [
+    [
+        "nama" => "chaerul anwar",
+        "nim" => "15200135",
+        "email" => "chaerulanwar428@gmail.com",
+        "jurusan" => "ilmu komputer"
+    ],
+    [
+        "nama" => "joko",
+        "nim" => "123456",
+        "email" => "joko@gmail.com",
+        "jurusan" => "ilmu kedokteran"
+    ]
+    ];
+
+?>
