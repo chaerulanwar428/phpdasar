c8c925d1f86f33cff7cbf243cde4ab2c5f5706dc
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-20 21:45:00 +0700

    menambahkan logic pada form agar bisa berjalan

--- materi09/login/login.php
+++ materi09/login/login.php
@@ -1,3 +1,18 @@
+<?php
+//cek apakah tombol submit sudah ditekan atau belum
+if (isset($_POST["submit"])){
+    //cek usernaem & password
+    if ($_POST ["username"] == "admin" && $_POST["password"] == "123"){
+        //jika benar, redicet ke halaman admin
+        header("Location: admin.php");
+        exit;
+    } else{
+        //jika salah,tampilkan pesan kesalah
+        $error = true;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +23,9 @@
 </head>
 <body>
     <h1>Login Admin</h1>
+<?php if (isset($error)) : ?>
+    <p style="color: red; font-style: italic;">username/ password salah</p>
+<?php endif; ?>
     <ul>
         <form action="" method="post">
         <li>
@@ -19,7 +37,7 @@
             <input type="password" name="password" id="password">
         </li>
         <li>
-            <button type="submit" > Login </button>
+            <button type="submit" name="submit"> Login </button>
         </li>
         </form>
     </ul>
