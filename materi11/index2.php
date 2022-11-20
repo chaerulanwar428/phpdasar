b2b6fb6396b77c12ae501da64cfdf7aac1f07b73
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-21 06:36:07 +0700

    menambahkan tabel pada index02

---
+++ materi11/index2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+<h1>Daftar Mahasiswa</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>
+            <a href="">ubah</a> |
+            <a href="">hapus</a>
+        </td>
+        <td><img src="img/img1.jpg" width="50"></td>
+        <td>15200135</td>
+        <td>chaerul anwar</td>
+        <td>chaerulanwar428@gmail.com</td>
+        <td>ilmu komputer</td>
+    </tr>
+</table>
+</body>
+</html>
