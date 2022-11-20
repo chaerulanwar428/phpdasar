04d062f464126ab830e6012826ba4b252ca03add
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-20 19:41:40 +0700

    menerapkan get pada website

--- materi09/latihan1.php
+++ materi09/latihan1.php
@@ -16,5 +16,21 @@ $mahasiswa = [
         "gambar" => "img2.jpg"
     ]
     ];
-    ?>
-    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get</title>
+</head>
+<body>
+    <?php foreach ( $mahasiswa as $mhs ) : ?>
+        <li>
+        <a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nim=<?= $mhs["nim"]; ?>&email=<?= $mhs["email"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>&gambar<?= $mhs["gambar"]; ?>"><? $mhs["nama"]; ?> </a>
+    </li>
+    <?php endforeach; ?>
+</body>
+</html>
