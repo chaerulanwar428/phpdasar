dd4ab46cffb886413784f601408912b4ddfd5631
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-20 19:12:34 +0700

    menambahkan data mahasiswa

---
+++ materi09/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "chaerul anwar",
+        "nim" => "15200135",
+        "email" => "chaerulanwar428@gmail.com",
+        "jurusan" => "ilmu komputer",
+        "gambar" => "img1.jpg"
+    ],
+    [
+        "nama" => "joko",
+        "nim" => "123456",
+        "email" => "joko@gmail.com",
+        "jurusan" => "ilmu kedokteran",
+        "gambar" => "img2.jpg"
+    ]
+    ];
+    ?>
+    
