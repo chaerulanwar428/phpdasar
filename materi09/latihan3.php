29bf1dfdf290dd64acffabe5bc5f121ca1e1a1b1
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-20 20:57:07 +0700

    menerapkan metode post

--- materi09/latihan3.php
+++ materi09/latihan3.php
@@ -7,10 +7,12 @@
     <title>POST</title>
 </head>
 <body>
-    
+<?php if (isset ($_POST["submit"])) : ?>
+    <h1>Halo , Selamat datang <?= $_POST["nama"]; ?></h1>
+<?php endif; ?>
 <form action="" method="post">
     Masukan nama:
-    <input type="text" namae="nama">
+    <input type="text" name="nama">
     <br>
     <button type="submit" name="submit"> Kirim!</button>
 </form>
