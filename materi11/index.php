67a53a4610cc1361c0407b15df805a00eef17a61
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-21 06:29:59 +0700

    menambahkan tabel

---
+++ materi11/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    
+<h1>Daftar Mahasiswa</h1>
+
+<table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>
+            <a href="">ubah</a> |
+            <a href="">hapus</a>
+        </td>
+        <td><img src="   "></td>
+    </tr>
+</table>
+</body>
+</html>
