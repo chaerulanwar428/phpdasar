7b3446756625d30278102b90ede5f324b46cb888
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-22 15:23:02 +0700

    menambahkan form untuk detail mahasiswa

--- materi12/tambah.php
+++ materi12/tambah.php
@@ -7,6 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Tambah mahasiswa</h1>
+    <form action="" method="mahasiswa">
+    <ul>
+        <li>
+            <label for="nim">NIM :</label>
+            <input type="text" name="nim" id="nim">
+        </li>
+    </ul>
+    </form>
 </body>
 </html>
