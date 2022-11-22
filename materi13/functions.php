43bc361e699c7b2b0ee7dafea7a97897ec1b58b3
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-23 01:15:40 +0700

    menambahkan logika di functions dan melengkapi beberapa file lainnya

--- materi13/functions.php
+++ materi13/functions.php
@@ -35,4 +35,28 @@ function hapus($id){
     mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id");
     return mysqli_affected_rows($conn);
 }
+
+function ubah($data){
+    global $conn;
+
+    $id = $data["id"];
+    $nim = htmlspecialchars($data["nim"]);
+    $nama = htmlspecialchars($data["nama"]);
+    $email = htmlspecialchars($data["email"]);
+    $jurusan = htmlspecialchars($data["jurusan"]);
+    $gambar   = htmlspecialchars($data["gambar"]);
+
+
+    $query = "UPDATE mahasiswa SET 
+                nim = '$nim',
+                nama = '$nama',
+                email = '$email',
+                jurusan = '$jurusan',
+                gambar = '$gambar'
+                WHERE id = $id
+                 ";
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn); 
+} 
 ?>
--- materi13/ubah.php
+++ materi13/ubah.php
@@ -39,6 +39,7 @@ if ( isset($_POST["submit"]) ){
 <body>
     <h1>Ubah data mahasiswa</h1>
     <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $mhs["id"]; ?>">
     <ul>
         <li>
             <label for="nim">NIM :</label>
@@ -46,19 +47,19 @@ if ( isset($_POST["submit"]) ){
         </li>
         <li>
             <label for="nama">Nama :</label>
-            <input type="text" name="nama" id="nama" required value="<?= $mhs["nama"]; ?>>
+            <input type="text" name="nama" id="nama" required value="<?= $mhs["nama"]; ?>">
         </li> 
         <li>
             <label for="email">Email :</label>
-            <input type="text" name="email" id="email" required value="<?= $mhs["email"]; ?>>
+            <input type="text" name="email" id="email" required value="<?= $mhs["email"]; ?>">
         </li>
         <li>
             <label for="jurusan">Jurusan :</label>
-            <input type="text" name="jurusan" id="jurusan" required value="<?= $mhs["jurusan"]; ?>>
+            <input type="text" name="jurusan" id="jurusan" required value="<?= $mhs["jurusan"]; ?>">
         </li>
         <li>
             <label for="gambar">Gambar :</label>
-            <input type="text" name="gambar" id="gambar" required value="<?= $mhs["gambar"]; ?>>
+            <input type="text" name="gambar" id="gambar" required value="<?= $mhs["gambar"]; ?>">
         </li>
         <li>
             <button type="submit" name="submit">Ubah data!</button>
