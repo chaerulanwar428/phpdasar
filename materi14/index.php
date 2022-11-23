2343d750991e8003b02bcce41eac5088f20cdbce
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-23 15:27:46 +0700

    menambahkan logika searching

--- materi14/index.php
+++ materi14/index.php
@@ -1,6 +1,12 @@
 <?php
 require 'functions.php';
 $mahasiswa = query ("SELECT * FROM mahasiswa");
+
+//tombol cari ditekan
+if (isset ($_POST["cari"])) {
+    $mahasiswa = cari ($_POST["keyword"]);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +20,13 @@ $mahasiswa = query ("SELECT * FROM mahasiswa");
     
 <h1>Daftar Mahasiswa</h1>
 <a href="tambah.php">Tambah data mahasiswa</a>
+<br><br>
+
+<form action="" method="post">
+<input type="text" name="keyword" size="40" autofocus placeholder="masukan keyword pencarian!!!" autocomplete="off">
+<button type="submit" name="cari" >Cari!</button>
+
+</form>
 
 <table border="1" cellpadding="10" cellspacing="0">
     <tr>
