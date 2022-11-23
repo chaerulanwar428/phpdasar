8a456384b3fe49f88b287c4ce7431997236e705a
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-23 15:04:45 +0700

    menambah form untuk input search

--- materi13/index.php
+++ materi13/index.php
@@ -13,7 +13,16 @@ $mahasiswa = query ("SELECT * FROM mahasiswa");
 <body>
     
 <h1>Daftar Mahasiswa</h1>
+
 <a href="tambah.php">Tambah data mahasiswa</a>
+<br><br>
+
+<form action="" method="post">
+<input type="text" name="keyword" size="40" autofocus placeholder="masukan keyword pencarian!!!" autocomplete="off">
+<button type="submit" name="cari" >Cari!</button>
+
+</form>
+<br>
 
 <table border="1" cellpadding="10" cellspacing="0">
     <tr>
