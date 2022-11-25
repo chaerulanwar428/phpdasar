66466e02e36e985452baddad7c1f95eaace0437f
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-26 00:47:07 +0700

    menmabahkan logika pada regitsrasi

--- materi16/registrasi.php
+++ materi16/registrasi.php
@@ -1,3 +1,17 @@
+<?php
+require 'functions.php';
+
+if(isset($_POST["register"])){
+
+    if(registrasi($_POST)> 0){
+        echo "<script> 
+        alert('user baru berhasil ditambahkan');
+        </script>";
+    }else{
+        echo mysqli_error($conn);
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
