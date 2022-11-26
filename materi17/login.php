a6c243f8deb9c36ba0b11b2f48e63ffa0dd663ed
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-26 09:14:59 +0700

    menambahkan file login yang sudah ada kerangka nya

---
+++ materi17/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+</head>
+<body>
+    <h1>halaman Login</h1>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username :</label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password :</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="login">Login!</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
