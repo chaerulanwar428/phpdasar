97da1a0144a6bdd8281a48b32a3c7c0768934ea1
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-30 05:27:22 +0700

    menambahkan logika dalam script.js

--- materi21/js/script.js
+++ materi21/js/script.js
@@ -2,3 +2,17 @@
 var keyword = document.getElementById('keyword');
 var tombolCari = document.getElementById('tombol-cari');
 var container = document.getElementById('container');
+
+//tambahkan event ketika keyword ditulis
+keyword.addEventListener('keyup', function () {
+  //buat kesiapan ajax
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      container.innerHTML = xhr.responseText;
+    }
+  };
+
+  //eksukusi ajax
+  xhr.open('GET', 'ajax/mahasiswa.php?keyword=' + keyword.value, true);
+  xhr.send();
+});
