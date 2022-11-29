e56e96b2bd11478e961b3217903f6369432fc8bf
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-30 05:21:09 +0700

    pembuatan variabel di javascript menggunakan DOM

---
+++ materi21/js/script.js
@@ -0,0 +1,4 @@
+//ambil elemen-elemen yang dibutuhkan
+var keyword = document.getElementById('keyword');
+var tombolCari = document.getElementById('tombol-cari');
+var container = document.getElementById('container');
