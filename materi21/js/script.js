33f1d614c2baedd269a21a3608c8c29e2b2330f7
chaerul anwar <chaerulanwar428@gmail.com>
2022-11-30 21:03:50 +0700

    menambahkan logika dengan penggunaanajax

--- materi21/js/script.js
+++ materi21/js/script.js
@@ -1,21 +1,35 @@
-//ambil elemen-elemen yang dibutuhkan
-var keyword = document.getElementById('keyword');
-var tombolCari = document.getElementById('tombol-cari');
-var container = document.getElementById('container');
+$(document).ready(function () {
+  //hilangkan tombol cari
+  $('#tombol-cari').hide();
 
-//buat object ajax
-var xhr = new XMLHttpRequest();
+  //event ketika keyword ditulis
+  $('#keyword').on('keyup', function () {
+    //munculkan icon loading
+    $('.loader').show();
 
-//tambahkan event ketika keyword ditulis
-keyword.addEventListener('keyup', function () {
-  //buat kesiapan ajax
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      container.innerHTML = xhr.responseText;
-    }
-  };
-
-  //eksukusi ajax
-  xhr.open('GET', 'ajax/mahasiswa.php?keyword=' + keyword.value, true);
-  xhr.send();
+    //ajax menggunakan load
+    $('container').load('ajax/mahasiswa.php?keyword=' + $('#keyword').val());
+  });
 });
+
+// //ambil elemen-elemen yang dibutuhkan
+// var keyword = document.getElementById('keyword');
+// var tombolCari = document.getElementById('tombol-cari');
+// var container = document.getElementById('container');
+
+// //buat object ajax
+// var xhr = new XMLHttpRequest();
+
+// //tambahkan event ketika keyword ditulis
+// keyword.addEventListener('keyup', function () {
+//   //buat kesiapan ajax
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//       container.innerHTML = xhr.responseText;
+//     }
+//   };
+
+//   //eksukusi ajax
+//   xhr.open('GET', 'ajax/mahasiswa.php?keyword=' + keyword.value, true);
+//   xhr.send();
+// });
